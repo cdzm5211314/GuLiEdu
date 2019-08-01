d86ff9c10d72c2f43025dfbb3e0a46635d12fbab
cdzm5211314 <123309778@qq.com>
2019-08-01 17:10:42 +0800

    机构列表页模版展示与数据填充

--- apps/orgs/urls.py
+++ apps/orgs/urls.py
@@ -1,6 +1,10 @@
 from django.conf.urls import url
-
+from orgs import views
 
 urlpatterns = [
+
+    # 机构列表
+    url(r'^org_list/$',views.org_list,name='org_list'),
+
 
 ]
--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from orgs.models import OrgInfo,TeacherInfo,CityInfo
 
 # Create your views here.
+
+# 机构列表
+def org_list(request):
+    all_orgs = OrgInfo.objects.all()  # 查询所有的机构信息
+    all_citys = CityInfo.objects.all()  # # 查询所有的城市信息
+    sort_orgs = all_orgs.order_by('-love_num')[:3]  # 按照收藏数降序排序机构并获取收藏数最高的三个机构
+    return render(request,'org-list.html',{
+        'all_orgs':all_orgs,
+        'all_citys':all_citys,
+        'sort_orgs':sort_orgs,
+    })
+
+
