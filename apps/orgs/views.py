2fa0cd851f6206cab1c0126adfe94d2fe01cd994
cdzm5211314 <123309778@qq.com>
2019-08-02 16:18:19 +0800

    机构列表按照类别或地区及类别和地区进行筛选过滤

--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -10,9 +10,20 @@ def org_list(request):
     all_citys = CityInfo.objects.all()  # 查询所有的城市信息
     sort_orgs = all_orgs.order_by('-love_num')[:3]  # 按照收藏数降序排序机构并获取收藏数最高的三个机构
 
+    # 根据机构类别进行筛选过滤
+    cate = request.GET.get('cate','')
+    if cate:
+        all_orgs = all_orgs.filter(category=cate)
+
+    # 根据所在地区进行筛选过滤
+    cityid = request.GET.get('cityid','')
+    if cityid:
+        all_orgs = all_orgs.filter(cityinfo_id=int(cityid))
+
+
+    # 分页显示
     page_num = request.GET.get('page_num','')  # 获取url传递过来的页码数值,默认值为1,可自定义
     paginator = Paginator(all_orgs,3)  # 创建分页对象,设置每页显示几条数据
-    # 分页
     try:
         pages = paginator.page(page_num)  # 获取页码值对应的分页对象
     except PageNotAnInteger:  # 页码不是整数时引发该异常
@@ -32,6 +43,8 @@ def org_list(request):
         'pages':pages,
         'all_citys':all_citys,
         'sort_orgs':sort_orgs,
+        'cate':cate,
+        'cityid':cityid,
     })
 
 
