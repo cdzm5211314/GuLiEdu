b7061280bbd7573986ca310278678b37b51103cd
cdzm5211314 <123309778@qq.com>
2019-08-06 16:38:26 +0800

    用户咨询功能Ajax实现及自定义校验规则

--- apps/operations/urls.py
+++ apps/operations/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
-
+from operations import views
 
 urlpatterns = [
+    # 我要学习的ajax请求咨询
+    url(r'^user_ask/$', views.user_ask, name='user_ask'),
 
 ]
--- apps/operations/views.py
+++ apps/operations/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from operations.forms import UserAskForm
+from django.http import JsonResponse
 
 # Create your views here.
+
+# 我要学习的ajax请求咨询
+def user_ask(request):
+    user_ask_form = UserAskForm(request.POST)
+    if user_ask_form.is_valid():  # 数据验证通过
+        user_ask_form.save(commit=True)
+        return JsonResponse({'status':'ok', 'msg':'咨询成功!!!'})
+    else:
+        return JsonResponse({'status':'fail', 'msg':'咨询失败!!!'})
+
+
+
+
+
+
+
+
+
+
