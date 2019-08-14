eb99dbae4a0a4ebbb6f956e4d4f05ae16b7b4217
cdzm5211314 <123309778@qq.com>
2019-08-14 17:38:15 +0800

    机构详情页首页的数据展示

--- apps/orgs/views.py
+++ apps/orgs/views.py
@@ -6,7 +6,7 @@ from django.core.paginator import Paginator,PageNotAnInteger,EmptyPage
 
 # 机构列表
 def org_list(request):
-    all_orgs = OrgInfo.objects.all()  # 查询所有的机构信息
+    all_orgs = OrgInfo.objects.all().order_by("id")  # 查询所有的机构信息
     all_citys = CityInfo.objects.all()  # 查询所有的城市信息
     sort_orgs = all_orgs.order_by('-love_num')[:3]  # 按照收藏数降序排序机构并获取收藏数最高的三个机构
 
